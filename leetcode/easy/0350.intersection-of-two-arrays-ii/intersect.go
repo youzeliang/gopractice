a9f2f43b8ca660146e1e42567877f42e1135398d
youzeliang <youzel@126.com>
2019-08-21 01:13:08 +0800

    217 给定一个整数数组，判断是否存在重复元素。如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。

---
+++ leetcode/easy/0350.intersection-of-two-arrays-ii/intersect.go
@@ -0,0 +1,19 @@
+package _350_intersection_of_two_arrays_ii
+
+/**
+
+https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+
+给定两个数组，编写一个函数来计算它们的交集。
+
+
+如果给定的数组已经排好序呢？你将如何优化你的算法？
+如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+如果 nums2 的元素存储在磁盘上，磁盘内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+*/
+
+func intersect(nums1 []int, nums2 []int) []int {
+	res := []int{}
+
+}
