583060d99aec09d5d3d9541b5dbc0174345d6dcb
youzeliang <youzel@126.com>
2019-08-22 00:51:01 +0800

    num 0122

---
+++ leetcode/easy/0122.best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.go
@@ -0,0 +1,49 @@
+package _122_best_time_to_buy_and_sell_stock_ii
+
+/**
+
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+
+设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+
+
+输入: [7,1,5,3,6,4]
+输出: 7
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+     随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+
+
+
+
+题解
+
+求出相邻两天的股票差值，当差值大于0的时候，完成一次交易
+
+对这个子数组
+ak，ak+1 ....
+
+
+
+*/
+
+func maxProfit(prices []int) int {
+
+	if len(prices) == 0 {
+		return 0
+	}
+
+	sum := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			sum += prices[i] - prices[i-1]
+
+		}
+	}
+
+	return sum
+}
