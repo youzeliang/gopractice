7b3eccec860d7a9cfce2ba026e1289517409a5bc
youzeliang <youzel@126.com>
2019-08-23 00:01:03 +0800

    update

--- leetcode/easy/0350.intersection-of-two-arrays-ii/intersect.go
+++ leetcode/easy/0350.intersection-of-two-arrays-ii/intersect.go
@@ -15,5 +15,43 @@ https://leetcode.com/problems/intersection-of-two-arrays-ii/
 
 func intersect(nums1 []int, nums2 []int) []int {
 	res := []int{}
+	m1 := getInts(nums1)
+	m2 := getInts(nums2)
 
+	if len(m1) > len(m2) {
+		m1, m2 = m2, m1
+	}
+	// m1 是较短的字典，会快一些
+	for n := range m1 {
+		m1[n] = min(m1[n], m2[n])
+	}
+
+	for n, size := range m1 {
+		for i := 0; i < size; i++ {
+			res = append(res, n)
+		}
+	}
+
+	return res
+
+}
+
+// 清理重复的值，也便于查询
+
+func getInts(a []int) map[int]int {
+
+	res := make(map[int]int, len(a))
+
+	for i := range a {
+		res[a[i]]++
+	}
+	return res
+}
+
+func min(a, b int) int {
+
+	if a < b {
+		return a
+	}
+	return b
 }
