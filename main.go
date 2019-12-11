0d71fbef12de35d92e0e12999c5586b762507481
youze <liangyouze@megvii.com>
2019-12-11 11:36:00 +0800

    common-divisor

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+
 
 func fibonacci(c, quit chan int) {
 	x, y := 1, 1
@@ -14,6 +19,9 @@ func fibonacci(c, quit chan int) {
 		}
 	}
 }
+
+var ch chan int  = make(chan int)
+
 
 func main() {
 	//c := make(chan int)
@@ -37,10 +45,44 @@ func main() {
 	//fmt.Printf("Here is the string s: %s\n", s) // prints same string
 
 
-	n := 0
-	reply := &n
-	Multiply(10, 5, reply)
-	fmt.Println("Multiply:", &reply) // Multiply: 50
+	//n := 0
+	//reply := &n
+	//Multiply(10, 5, reply)
+	//fmt.Println("Multiply:", &reply) // Multiply: 50
+
+	testChan()
+}
+
+func testChan()  {
+	var ch2 chan string
+	ch2 = make(chan string)
+	var ch3 chan string
+	ch3 = make(chan string, 1)
+
+	ch4 := make(chan float32)
+	ch5 := make(chan float64, 2)
+	fmt.Printf("无缓冲 局部变量 chan ch2 : %v\n", ch2)
+	fmt.Printf("有缓冲 局部变量 chan ch3 : %v\n", ch3)
+	fmt.Printf("无缓冲 局部变量 chan ch4 : %v\n", ch4)
+	fmt.Printf("有缓冲 局部变量 chan ch5 : %v\n", ch5)
+}
+
+func TestSlice()  {
+
+	m := map[string]string{"q": "q", "w": "w", "e": "e", "r": "r", "t": "t", "y": "y"}
+	var slice []string
+	for k, _ := range m {
+		slice = append(slice, k)
+	}
+	fmt.Printf("clise string is : %v\n", slice)
+	sort.Strings(slice)
+	fmt.Printf("sorted slice string is : %v\n", slice)
+	for _, v := range slice {
+		fmt.Println(m[v])
+	}
+
+
+
 }
 
 // this function changes reply:
---
+++ others/number/common-divisor.go
@@ -0,0 +1,89 @@
+package number
+
+// greatest common divisor
+
+//欧几里得辗转相除法：
+//
+//gcd(x,y)表示x和y的最大公约数
+//
+//进入运算时:x!=0,y!=0，本质上就是不断转换成求等价更小数的最大公约数。如果x%y=0，返回y，即最大公约数。
+//gcd(x,y)=gcd(y,x%y)
+//
+//证明：
+//设k=x/y，b=x%y  则：x=ky+b
+//如果n能够同时整除x和y，则(y%n)=0,(ky+b)%n=0，则b%n=0，即n也同时能够整除y和b。
+//由上得出：同时能够整除y和(b=x%y)的数，也必然能够同时整除x和y。故而gcd(x,y)=gcd(y,x%y)。当(b=x%y)=0，即y可以整除x，这时的y也就是所求的最大公约数了。
+//
+//附上两条重要性质：gcd(a,b)=gcd(b,a)，gcd(-a,b)=gcd(a,b)
+
+/*
+*穷举法：最大公约数
+ */
+func gcdNormal(x, y int) int {
+	var n int
+	if x > y {
+		n = y
+	} else {
+		n = x
+	}
+	for i := n; i >= 1; i-- {
+		if x%i == 0 && y%i == 0 {
+			return i
+		}
+	}
+	return 1
+}
+
+/*
+*辗转相除法：最大公约数
+*递归写法，进入运算是x和y都不为0
+ */
+func gcd(x, y int) int {
+	tmp := x % y
+	if tmp > 0 {
+		return gcd(y, tmp)
+	} else {
+		return y
+	}
+}
+
+/*
+*辗转相除法：最大公约数
+*非递归写法
+ */
+func gcdx(x, y int) int {
+	var tmp int
+	for {
+		tmp = (x % y)
+		if tmp > 0 {
+			x = y
+			y = tmp
+		} else {
+			return y
+		}
+	}
+}
+
+/*
+*穷举写法：最小公倍数
+ */
+func lcmNormal(x, y int) int {
+	var top int = x * y
+	var i = x
+	if x < y {
+		i = y
+	}
+	for ; i <= top; i++ {
+		if i%x == 0 && i%y == 0 {
+			return i
+		}
+	}
+	return top
+}
+
+/*
+*公式解法：最小公倍数=两数之积/最大公约数
+ */
+func lcm(x, y int) int {
+	return x * y / gcd(x, y)
+}
