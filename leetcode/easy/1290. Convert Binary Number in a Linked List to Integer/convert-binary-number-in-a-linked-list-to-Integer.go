8eefc6bdb17076fcdea1da07471bb95b6aee80ec
youze <liangyouze@megvii.com>
2019-12-18 14:28:52 +0800

    update

--- leetcode/easy/1290. Convert Binary Number in a Linked List to Integer/convert-binary-number-in-a-linked-list-to-Integer.go
+++ leetcode/easy/1290. Convert Binary Number in a Linked List to Integer/convert-binary-number-in-a-linked-list-to-Integer.go
@@ -1,23 +1,24 @@
 package _290__Convert_Binary_Number_in_a_Linked_List_to_Integer
 
-
 /**
 
 https://leetcode-cn.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+
+遍历链表，将每次遍历到的元素添加到结果的末尾
 
  * Definition for singly-linked list.
  * type ListNode struct {
  *     Val int
  *     Next *ListNode
  * }
- */
+*/
 func getDecimalValue(head *ListNode) int {
 
-	num :=0
-	if head!=nil{
-		num<<=1
-		num+=head.Val
-		head=head.Next
+	num := 0
+	if head != nil {
+		num <<= 1
+		num += head.Val
+		head = head.Next
 	}
 
 	return num
