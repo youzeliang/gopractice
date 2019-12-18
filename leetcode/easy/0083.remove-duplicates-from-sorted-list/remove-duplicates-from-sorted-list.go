be3b5cdb0ab9c490052d4053b0772eb0cc0a3775
youze <youzel@126.com>
2019-12-18 08:50:13 +0800

    update

---
+++ leetcode/easy/0083.remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
@@ -0,0 +1,16 @@
+package _083_remove_duplicates_from_sorted_list
+
+// ListNode is singly-linked list
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+
+	if head == nil {
+		return nil
+	}
+
+	return nil
+}
