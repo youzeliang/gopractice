d3a6cf0fc982295de53fab58e9ed811dc582968e
youze <youzel@126.com>
2020-04-28 23:09:39 +0800

    publish book

--- leetcode/easy/0026.remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
+++ leetcode/easy/0026.remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
@@ -44,3 +44,15 @@ func removeDuplicates(nums []int) int {
 
 	return j
 }
+
+func removeDuplicates2(nums []int) int {
+	for i := 0; i+1 < len(nums); {
+		if nums[i] == nums[i+1] {
+			nums = append(nums[:i], nums[i+1:]...)
+		} else {
+			i++
+		}
+	}
+
+	return len(nums)
+}
--- leetcode/easy/0027.remove-element/remove-element.go
+++ leetcode/easy/0027.remove-element/remove-element.go
@@ -11,3 +11,15 @@ func removeElement(nums []int, val int) int {
 
 	return result
 }
+
+func removeElement2(nums []int, val int) int {
+	for i := 0; i < len(nums); {
+		if nums[i] == val {
+			nums = append(nums[:i], nums[i+1:]...)
+		} else {
+			i++
+		}
+	}
+
+	return len(nums)
+}
