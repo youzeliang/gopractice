bd2b4d6f08e621c80f3fce4f48fb5d7c9f076613
youze <youzel@126.com>
2020-04-29 14:28:59 +0800

    publish book

---
+++ leetcode/middle/0050.powx-n/powx-n.go
@@ -0,0 +1,26 @@
+package _050_powx_n
+
+func myPow(x float64, n int) float64 {
+	if n < 0 {
+		return 1.0 / pow(x, -n)
+	}
+
+	return pow(x, n)
+}
+
+func pow(x float64, n int) float64 {
+	if x == 0 {
+		return 0
+	}
+
+	if n == 0 {
+		return 1
+	}
+
+	res := pow(x, n>>1)
+	if n&1 == 0 {
+		return res * res
+	}
+
+	return res * res * x
+}
