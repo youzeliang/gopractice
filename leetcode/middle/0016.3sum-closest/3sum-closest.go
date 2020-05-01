b7198fe8b1d5a81fa16380d9cd38b25a3fe4eec6
youze <youzel@126.com>
2020-05-01 10:18:07 +0800

    publish book

--- leetcode/middle/0016.3sum-closest/3sum-closest.go
+++ leetcode/middle/0016.3sum-closest/3sum-closest.go
@@ -1,43 +1,40 @@
 package _016_3sum_closest
 
 import (
-	"math"
 	"sort"
 )
 
 func threeSumClosest(nums []int, target int) int {
-	// 排序后，可以按规律查找
+	res := nums[0] + nums[1] + nums[2]
+
 	sort.Ints(nums)
-	res, delta := 0, math.MaxInt64
 
-	for i := range nums {
-		// 避免重复计算
-		if i > 0 && nums[i] == nums[i-1] {
-			continue
-		}
-
-		l, r := i+1, len(nums)-1
-
-		for l < r {
-			s := nums[i] + nums[l] + nums[r]
-			switch {
-			case s < target:
-				l++
-				if delta > target-s {
-					delta = target - s
-					res = s
-				}
-			case s > target:
-				r--
-				if delta > s-target {
-					delta = s - target
-					res = s
-				}
-			default:
-				return s
+	for i := 0; i < len(nums)-2; i++ {
+		left, right := i+1, len(nums)-1
+		for left < right {
+			tem := nums[i] + nums[left] + nums[right]
+			if tem > target {
+				right--
+			} else if tem < target {
+				left++
+			} else {
+				return target
 			}
+			if distance(tem, target) < distance(res, target) {
+				res = tem
+			}
+
 		}
 	}
 
 	return res
+
+}
+
+func distance(a, b int) int {
+	if a < b {
+		return b - a
+	}
+
+	return a - b
 }
