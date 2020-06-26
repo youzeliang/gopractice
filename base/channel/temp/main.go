22fd5ab0061f57a4588ecd730abc809fa1ff0031
youze <youzel@126.com>
2020-06-26 16:49:04 +0800

    commit

---
+++ base/channel/temp/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s, (i+1)*100)
+	}
+}
+func say2(s string,ch chan int) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(150 * time.Millisecond)
+		fmt.Println(s, (i+1)*150)
+	}
+
+	ch <- 0
+	close(ch)
+}
+func main() {
+
+	c := make(chan int)
+	go say2("world",c)
+	say("hello------------------")
+	fmt.Println(<-c)
+}
