48b10f59944d77319450157e0af37d09e807f7f4
youze <youzel@126.com>
2020-08-22 22:32:05 +0800

    update

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 type F struct {
-	Name string 
+	Name string
 }
 
 func NewF(name string) *F {
@@ -14,16 +14,9 @@ func NewF(name string) *F {
 }
 
 func main() {
-<<<<<<< HEAD
 	fmt.Println(1111)
-}
-=======
-	a := Float64bits(11.1)
-	fmt.Println(a)
 }
 
 func Float64bits(f float64) uint64 {
 	return *(*uint64)(unsafe.Pointer(&f))
 }
-
->>>>>>> ae9288816f12c72a6ac09705300e487a2e3ef736
