41a14e389ac3a4b1dee840ad718af58c1e20877f
youze <youzel@126.com>
2020-09-05 08:09:19 +0800

    update

---
+++ leetcode/easy/0437.path-sum-iii/path-sum-iii.go
@@ -0,0 +1,36 @@
+package _437_path_sum_iii
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func pathSum(root *TreeNode, sum int) int {
+	if root == nil {
+		return 0
+	}
+
+	cnt := 0
+
+	var dfs func(*TreeNode, int)
+	dfs = func(node *TreeNode, sum int) {
+		if node == nil {
+			return
+		}
+
+		sum -= node.Val
+		if sum == 0 {
+			cnt++
+		}
+
+		dfs(node.Left, sum)
+		dfs(node.Right, sum)
+	}
+
+	dfs(root, sum)
+
+	return cnt +
+		pathSum(root.Left, sum) +
+		pathSum(root.Right, sum)
+}
