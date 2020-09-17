33687de20386d66af5343da96b59e52a8e4c96e9
youze <youzel@126.com>
2020-09-18 01:15:39 +0800

    update

--- publish.sh
+++ publish.sh
@@ -1,9 +1,2 @@
-# shellcheck disable=SC2035
-#git add * && git commit -a  -m  'update' && git push origin master
-
-#export GO_PATH=~/go
-#export PATH=$PATH:/$GO_PATH/bin
-#protoc --go_out=plugins=grpc:. grpc/warectu/warectu.proto
-
-ls -lR|grep "^d"|wc -l
+git add * && git commit -a  -m  'update' && git push origin master
 
