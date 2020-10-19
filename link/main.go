81f5ccad08fa8747f8f174581bd92dffc87e11e8
youze <youzel@126.com>
2020-10-19 11:30:39 +0800

    publish book

---
+++ link/main.go
@@ -0,0 +1,19 @@
+package link
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	res := &ListNode{
+		Val: 1,
+	}
+
+	if l1 == nil || l2 == nil {
+
+	}
+
+	return nil
+}
