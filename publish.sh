b01602491cbdbf07b0ec152a1334801bd8be6b63
youze <youzel@126.com>
2021-02-28 17:25:02 +0800

    update

--- publish.sh
+++ publish.sh
@@ -1,8 +1,2 @@
 #！/bin/bash
-
-#int=1
-#while(( $int<=100 ))
-#do
-# git add * && git commit -a  -m  'update' && git push origin master
-#    sleep 10
-#done
+git add * && git commit -a  -m  'update' && git push origin master
