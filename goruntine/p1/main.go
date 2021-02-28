1f286e7d10bf28a88c9e8394d352c5609c37ccf0
youze <youzel@126.com>
2021-02-28 18:50:02 +0800

    update

---
+++ goruntine/p1/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	g()
+}
+
+
+func g(){
+	var wg sync.WaitGroup
+
+	for i := 0; i <1000 ; i++ {
+		wg.Add(1)
+		go func(i int ,wg sync.WaitGroup) {
+			fmt.Println(i)
+			defer wg.Done()
+		}(i,wg)
+	}
+	wg.Wait()
+	fmt.Println("finished")
+
+	//time.Sleep(time.Second*10)
+}
+
+
+// waitGroup 的add 要在goroutine之前执行
