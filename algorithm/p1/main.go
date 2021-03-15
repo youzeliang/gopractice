a0a19e3ada504cbd1aff24bb36ce365e84daa410
youze <youzel@126.com>
2021-03-15 09:03:41 +0800

    update

--- algorithm/p1/main.go
+++ algorithm/p1/main.go
@@ -2,44 +2,141 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
 func main() {
 
+	ss()
+}
+
+func m(nums []int) int{
+
+	var count ,major int
+
+	for _,m := range nums{
+
+		if count == 0 {
+			major = m
+		}
+
+		if major == m {
+			count++
+		}else {
+			count--
+		}
 
 
-	//fmt.Println(runtime.NumCPU())
-	//
-	//
-	//numJewelsInStones("Add","Addccc")
 
-
-	//intersect(nil)
-
-	arr := []int{1, 2, 3}
-	for _, v := range arr {
-		arr = append(arr, v)
 	}
-	fmt.Println(arr)
+
+	return major
+
 
 }
 
-func consecutiveNumbersSum(N int) int {
+func ss() {
 
-	var res  = 1
-	var sum = 1
-	for i := 2;sum<=n;i++{
-		sum += i
-		if (N-sum) % i == 0 {
-			res++
+	s := [][]int{{1, 3},
+		{2, 6},
+		{15, 18},
+		{8, 10},
+	}
+
+	fmt.Println(s)
+
+	sort.Slice(s, func(i, j int) bool {
+		return s[i][0] < s[j][0]
+	})
+
+	fmt.Println(s)
+
+
+
+}
+
+func merge(intervals [][]int) [][]int {
+	if len(intervals) <= 0 {
+		return nil
+	}
+
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	var merged [][]int
+	merged = append(merged, intervals[0])
+
+	for i := 1; i < len(intervals); i++ {
+		m := merged[len(merged)-1]
+		c := intervals[i]
+
+		if c[0] > m[1] {
+			merged = append(merged, c)
+			continue
+		}
+
+		if c[1] > m[1] {
+			m[1] = c[1]
 		}
 	}
 
-	return res
+	return merged
 }
 
-func intersect(nums []int  )  {
+func canPartitionKSubsets(nums []int, k int) bool {
+	size := len(nums)
+	sum := 0
+	max := nums[0]
+	for _, n := range nums {
+		sum += n
+		if max < n {
+			max = n
+		}
+	}
+
+	if sum%k != 0 || sum/k < max {
+		// 提前结束
+		return false
+	}
+
+	// nums 降序排列可以加快 dfs 的速度
+	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+
+	// isUsed[i] == true 表示 nums[i] 已经被归类到某个小组，无法再使用了
+	isUsed := make([]bool, size)
+
+	return dfs(nums, isUsed, k, 0, 0, sum/k)
+}
+
+func dfs(nums []int, isUsed []bool, k, start, sum, target int) bool {
+	if k == 1 {
+		// 已经找到了 k-1 组解
+		// 剩下的自然就是第 k 组解
+		return true
+	}
+
+	if sum == target {
+		// 找到第 k 组的一种解
+		// 开始搜索 k-1 组的解
+		return dfs(nums, isUsed, k-1, 0, 0, target)
+	}
+
+	for i := start; i < len(nums); i++ {
+		if !isUsed[i] && sum+nums[i] <= target {
+			isUsed[i] = true
+			// 试着 sum+nums[i]
+			if dfs(nums, isUsed, k, i+1, sum+nums[i], target) {
+				return true
+			}
+			isUsed[i] = false
+		}
+	}
+
+	return false
+}
+
+func intersect(nums []int) {
 
 	s1 := "hello"
 	// 强制类型转换
@@ -96,4 +193,3 @@ func lengthOfLongestSubstring(s string) int {
 	}
 	return window
 }
-
---
+++ base/select/p2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"time"
+)
+
+var ch = make(chan int)
+
+func main() {
+	for i := 1; i <= 75; i++ {
+		go print(i)
+		ch <- 0
+	}
+	time.Sleep(5 * time.Second) //wait all goroutines
+}
+
+func print(i int) {
+	<-ch
+	fmt.Println(i)
+
+
+
+
+}
