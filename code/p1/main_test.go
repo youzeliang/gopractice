b4b87cc974c2b7d7628f12fbb4d948982a99ef2b
liangyouze <liangyouze@tal.com>
2021-04-25 00:08:16 +0800

    update

---
+++ code/p1/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"testing"
+)
+
+func TestName(t *testing.T) {
+
+	fmt.Println(411341)
+
+
+
+
+	str, err := json.Marshal(nil)
+	if err != nil {
+		errors.New("fdsfsd")
+	}
+
+
+
+
+
+}
+
