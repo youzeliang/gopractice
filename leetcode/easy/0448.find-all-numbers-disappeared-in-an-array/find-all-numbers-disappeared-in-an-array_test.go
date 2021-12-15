75135179a06a2c55d3a31185083bd862a746e1e3
youze <youzel@126.com>
2021-12-15 21:57:10 +0800

    publish book

---
+++ leetcode/easy/0448.find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array_test.go
@@ -0,0 +1,14 @@
+package _448_find_all_numbers_disappeared_in_an_array
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_findDisappearedNumbers(t *testing.T) {
+
+	var a = []int{1, 4, 1, 2}
+	fmt.Println(findDisappearedNumbers(a))
+}
+
+
