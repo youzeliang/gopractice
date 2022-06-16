f7167b54245031609c003c0abda3a5a6dd32bd28
liangyouze <liangyouze@tal.com>
2022-06-16 14:59:28 +0800

    update

--- a/p17/main.go
+++ a/p17/main.go
@@ -1,17 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 
-	var a = []int{1, 2, 0, 3}
+	var oneVOneCreate int64 =  math.MaxInt64
 
-	if len(a) > 3 {
-		for i := 1; i < len(a)-1; i++ {
-			if a[i-1] != 0 && a[i+1] != 0 && a[i] == 0 {
-				fmt.Println(a[i])
-				fmt.Println(i)
-			}
-		}
-	}
+	fmt.Println(oneVOneCreate)
+
 }
