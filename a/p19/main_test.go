1f52ecd19e656552e4457ce035e5a324fabdc131
liangyouze <liangyouze@tal.com>
2023-08-24 11:04:22 +0800

    update

---
+++ a/p19/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_getUsers(t *testing.T) {
+
+	fmt.Println(321)
+}
---
+++ base/channel/p18/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func longtimeCostFunc(ctx context.Context, c chan<- int) {
+	for i := 0; i < 10; i++ {
+		select {
+		case <-ctx.Done():
+			fmt.Println("calculate interrupted")
+			return
+		case <-time.After(time.Second):
+			fmt.Println("calculating...")
+		}
+	}
+	c <- 1
+	fmt.Println("calculate finished")
+}
+
+func main() {
+	result := make(chan int, 1)
+	ctx, cancel := context.WithCancel(context.Background())
+	go longtimeCostFunc(ctx, result)
+
+	select {
+	case r := <-result:
+		fmt.Println("longtimeCostFunc return:", r)
+	case <-time.After(3 * time.Second):
+		// notify worker goroutine to exit
+		cancel()
+		// handle timeout error
+		fmt.Println("too long to wait for the result")
+	}
+
+	// blocking main goroutine exit
+	time.Sleep(time.Minute)
+
+	return
+}
