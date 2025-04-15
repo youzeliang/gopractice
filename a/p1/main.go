2d98578bf54ba0eb7605d03a54574edddcb834ce
youze <youzel@126.com>
2025-04-15 10:18:09 +0800

    update log

--- a/p1/main.go
+++ a/p1/main.go
@@ -1,1 +1,23 @@
-package p1
+package main
+
+type Animal interface {
+	Speak() string
+}
+type Dog struct {
+}
+
+func (d Dog) Speak() string {
+	return "Woof!"
+
+}
+
+func makeSomething() {
+
+	var d = Dog{}
+	d.Speak()
+}
+
+func main() {
+
+	makeSomething()
+}
