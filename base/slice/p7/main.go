4482c5ba6ee80ef6ab5212c887125ad48c8b4292
youze <youzel@126.com>
2025-05-10 13:09:08 +0800

    update btree

--- base/slice/p7/main.go
+++ base/slice/p7/main.go
@@ -9,4 +9,8 @@ func main() {
 	fmt.Println("foo:", foo) // foo: [0 0 0 42 99]
 	fmt.Println("bar:", bar) // bar: [0 0 42 99]
 
+	//bar 的 cap 容量会到原始切片的末尾，所以当前 bar 的 cap 长度为 4。
+
+	//如果要解决这样的问题，其实可以在截取时指定容量：
+
 }
